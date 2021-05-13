ea9ffa8e2f63ef1557c98bd5fc9cc3f6a4188b4d
youssefhafez <youssefabdelazim@yahoo.com>
2021-05-13 23:06:35 +0200

    fixed

---
+++ build/precache-manifest.7839eff3fc1d7672b35cfbe7f06038b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa29bcb398c810671be1",
+    "url": "/MyReads/static/css/main.b6443193.chunk.css"
+  },
+  {
+    "revision": "aa29bcb398c810671be1",
+    "url": "/MyReads/static/js/main.aa29bcb3.chunk.js"
+  },
+  {
+    "revision": "87f84a5d270efee94a00",
+    "url": "/MyReads/static/js/1.87f84a5d.chunk.js"
+  },
+  {
+    "revision": "970c0060ff174c587b9c",
+    "url": "/MyReads/static/js/runtime~main.970c0060.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "ed7e99626f0dbf16e79b3392a1e2618d",
+    "url": "/MyReads/index.html"
+  }
+];
