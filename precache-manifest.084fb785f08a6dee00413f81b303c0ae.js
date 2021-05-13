732d713d5397357b64903503eb794e61018baa0f
youssefhafez <youssefabdelazim@yahoo.com>
2021-05-13 17:33:54 +0200

    Updates

--- precache-manifest.084fb785f08a6dee00413f81b303c0ae.js
+++ precache-manifest.084fb785f08a6dee00413f81b303c0ae.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "092bacf2eddf13eaf3b0",
+    "revision": "e780a65cb2d84eef283b",
     "url": "/my-app/static/css/main.394ca04e.chunk.css"
   },
   {
-    "revision": "092bacf2eddf13eaf3b0",
-    "url": "/my-app/static/js/main.092bacf2.chunk.js"
+    "revision": "e780a65cb2d84eef283b",
+    "url": "/my-app/static/js/main.e780a65c.chunk.js"
   },
   {
     "revision": "87f84a5d270efee94a00",
@@ -16,19 +16,19 @@ self.__precacheManifest = [
     "url": "/my-app/static/js/runtime~main.a3bd857c.js"
   },
   {
-    "revision": "bb9363b86b02777d23523851826414e0",
-    "url": "/my-app/static/media/add.bb9363b8.svg"
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-app/static/media/arrow-drop-down.78c6f53b.svg"
   },
   {
     "revision": "3687aa988bec28f17ae39153db1646c3",
     "url": "/my-app/static/media/arrow-back.3687aa98.svg"
   },
   {
-    "revision": "78c6f53b2342643639fe1cbaf171bf47",
-    "url": "/my-app/static/media/arrow-drop-down.78c6f53b.svg"
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-app/static/media/add.bb9363b8.svg"
   },
   {
-    "revision": "ab723cb449696b2424f617ecbf1048e5",
+    "revision": "3df82a2f0a3bf47af53f18e4236b86ab",
     "url": "/my-app/index.html"
   }
 ];
