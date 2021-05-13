750314fc9bd88bca15afb6e37144f39e0d110c0d
youssefhafez <youssefabdelazim@yahoo.com>
2021-05-13 17:23:43 +0200

    Updates

--- precache-manifest.995fa0a89f544a8a651db11b6b6ae700.js
+++ precache-manifest.995fa0a89f544a8a651db11b6b6ae700.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "549149f979065eab8d91",
+    "revision": "092bacf2eddf13eaf3b0",
     "url": "/my-app/static/css/main.394ca04e.chunk.css"
   },
   {
-    "revision": "549149f979065eab8d91",
-    "url": "/my-app/static/js/main.549149f9.chunk.js"
+    "revision": "092bacf2eddf13eaf3b0",
+    "url": "/my-app/static/js/main.092bacf2.chunk.js"
   },
   {
     "revision": "87f84a5d270efee94a00",
@@ -20,15 +20,15 @@ self.__precacheManifest = [
     "url": "/my-app/static/media/add.bb9363b8.svg"
   },
   {
-    "revision": "78c6f53b2342643639fe1cbaf171bf47",
-    "url": "/my-app/static/media/arrow-drop-down.78c6f53b.svg"
-  },
-  {
     "revision": "3687aa988bec28f17ae39153db1646c3",
     "url": "/my-app/static/media/arrow-back.3687aa98.svg"
   },
   {
-    "revision": "e65e2328e7f52c518390ab9a8cdede14",
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "ab723cb449696b2424f617ecbf1048e5",
     "url": "/my-app/index.html"
   }
 ];
