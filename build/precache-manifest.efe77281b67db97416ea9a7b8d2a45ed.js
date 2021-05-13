80c3fd1066d3007a5e3d9dd8f9650b1e4ff33eb7
youssefhafez <youssefabdelazim@yahoo.com>
2021-05-13 17:23:00 +0200

    first upload

---
+++ build/precache-manifest.efe77281b67db97416ea9a7b8d2a45ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "549149f979065eab8d91",
+    "url": "/my-app/static/css/main.394ca04e.chunk.css"
+  },
+  {
+    "revision": "549149f979065eab8d91",
+    "url": "/my-app/static/js/main.549149f9.chunk.js"
+  },
+  {
+    "revision": "87f84a5d270efee94a00",
+    "url": "/my-app/static/js/1.87f84a5d.chunk.js"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-app/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-app/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-app/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "e65e2328e7f52c518390ab9a8cdede14",
+    "url": "/my-app/index.html"
+  }
+];
